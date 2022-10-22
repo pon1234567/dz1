f1c0c54d48a5853aa7bfc866a9b63a9f489755a8
Admin <isukvoloda65@gmail.com>
2022-10-22 13:04:14 +0300

    добрий день

--- main.py
+++ main.py
@@ -1,17 +1,1 @@
-class Dog:
-    def __init__(self, name, age, breed, height):
-        self.name = name
-        self.age = age
-        self.breed = breed
-        self.height = height
-dog1=Dog(
-    "Ragnarok",
-    3,
-    "Doberman",
-    120
-)
-print(dog1.name)
-print(dog1.age)
-print(dog1.breed)
-print(dog1.height)
-
+print("Добрий День!")
