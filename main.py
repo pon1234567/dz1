55ef4832782960076015fd86a23e428d04b18e56
Admin <isukvoloda65@gmail.com>
2022-10-22 12:55:16 +0300

    додав ім'я собачки

---
+++ main.py
@@ -0,0 +1,13 @@
+class Dog:
+    def __init__(self, name, age, breed, height):
+        self.name = name
+        self.age = age
+        self.breed = breed
+        self.height = height
+dog1=Dog(
+    "Ragnarok",
+    3,
+    "Doberman",
+    120
+)
+print(dog1.name)
